9f4a19755d748f787dd9cd82158a3612d603b001
Aunik Hussain <31704@longroad.ac.uk>
2014-09-09 15:10:25 +0100

    First comment

---
+++ hello world2.py
@@ -0,0 +1,11 @@
+# Aunik Hussain
+# 09/09/2104
+# Exercise 1.1 Hello world
+
+print("hello world")
+# (below) Assign please enter your name to first_name
+first_name = input("please enter your name")
+# (below) Print what you write
+print(first_name)
+# (below) basically means retype what you typed
+print("Hi {0} !". format(first_name))
