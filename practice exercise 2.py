f5f5a0e10106a1316d5ade9e3c0fa727d9710e76
Aunik Hussain <31704@longroad.ac.uk>
2014-09-15 21:57:40 +0100

    practice exercise 2 commit

---
+++ practice exercise 2.py
@@ -0,0 +1,12 @@
+#Aunik Hussain
+#15-09-2104
+#Revision Exercise 2
+
+print("This programme will times together your first two numbers and divide it by the third")
+number1 = int(input("please enter the first number"))
+number2 = int(input("please enter the second number"))
+number3 = int(input("please enter the third number"))
+
+answer = number1 * number2 / number3
+print("The answer of this calculation is {0} * {1} / {2} is {3}.".format(number1,number2,number3,answer))
+
