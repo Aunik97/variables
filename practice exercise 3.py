f05cd25b501463cc17ba511909eaea4d28a1c63d
Aunik Hussain <31704@longroad.ac.uk>
2014-09-15 21:58:59 +0100

    practice exercise 3 commit

---
+++ practice exercise 3.py
@@ -0,0 +1,10 @@
+#Aunik Hussain
+#15-09-2014
+#Practice Exercise 3
+
+print("this programme will divide two numbers and show the remainder")
+number1 = int(input("please enter the first number"))
+number2 = int(input("please enter the second number"))
+answer = number1 / number2
+
+print("The answer of this calculation is {0} / {1} is {2}.".format(number1,number2,answer))
