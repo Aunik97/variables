5402bddd2a6327deac8e04055265e3691868430d
Aunik Hussain <31704@longroad.ac.uk>
2014-09-16 13:51:43 +0100

    practice 2

---
+++ practic2e.py
@@ -0,0 +1,11 @@
+#Aunik Hussain
+#15-09-2014
+#Practice Exercise 1
+
+print("please enter four numbers for addition")
+number1 = input("please enter first number")
+number2 = input("please enter second number")
+number3 = input("please enter third number")
+number4 = input("please enter fourth number")
+answer = number1 + number2 + number3 + number4
+int(print("The addition of these numbers is {0} + {1} + {2} + {3} = {4} ".format(number1,number2,number3,number4,answer)))
