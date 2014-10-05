0adcf1494d5868592e98d7f098211e8621c35dad
Aunik Hussain <31704@longroad.ac.uk>
2014-10-05 23:25:45 +0100

    practice

---
+++ finding the area of a circle.py
@@ -0,0 +1,13 @@
+#Aunik Hussain
+#05/10/2014
+#finding the circumference of a circle
+
+
+
+
+diameter = float(input("Please enter the diameter of the circle: "))
+
+circumference = diameter * 3.142
+
+print("The circumfence of your circle is {0}.".format(circumference, diameter))
+                 
